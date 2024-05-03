3ff55e7a3f9392c1f402fee3510f9dbb2ce4d2c4
Lucas Bourne <lucasbourne@Yepics-MBP.lan>
2024-05-03 11:23:35 +0100

    Finish section 4, add section 5

--- section-4-monster-game/monster-game/app.js
+++ section-4-monster-game/monster-game/app.js
@@ -10,6 +10,7 @@ Vue.createApp({
       roundCounter: 0,
       specialAttackResetRound: 0,
       winner: null,
+      battleLog: [],
     };
   },
   computed: {
@@ -28,20 +29,33 @@ Vue.createApp({
     canSpecialAttack() {
       return this.roundCounter >= this.specialAttackResetRound;
     },
+    canHeal() {
+      return this.playerHealth < 100;
+    },
   },
   watch: {
     playerHealth(value) {
       if (value <= 0 && this.monsterHealth <= 0) {
         this.winner = "draw";
+        this.battleLog.push(
+          "It's a draw both you and the monster take each other out!"
+        );
       } else if (value <= 0) {
         this.winner = "monster";
+        this.battleLog.push("Oh no! The monster delivers a crushing blow!");
       }
     },
     monsterHealth(value) {
       if (value <= 0 && this.playerHealth <= 0) {
         this.winner = "draw";
+        this.battleLog.push(
+          "It's a draw both you and the monster take each other out!"
+        );
       } else if (value <= 0) {
         this.winner = "player";
+        this.battleLog.push(
+          "Congratulations! You land a final attack and defeat the monster!"
+        );
       }
     },
   },
@@ -52,22 +66,32 @@ Vue.createApp({
       this.monsterHealth = 100;
       this.playerHealth = 100;
       this.winner = null;
+      this.battleLog = [];
     },
     attackMonster() {
       this.roundCounter++;
       const attackDamage = _getRandomValue(5, 12);
       this.monsterHealth -= attackDamage;
+      this.battleLog.push(
+        "You attack the monster, dealing " + attackDamage + " damage!"
+      );
       this.attackPlayer();
     },
     attackPlayer() {
       const attackDamage = _getRandomValue(8, 15);
       this.playerHealth -= attackDamage;
+      this.battleLog.push(
+        "Monster attacks you, dealing " + attackDamage + " damage!"
+      );
     },
     specialAttackMonster() {
       this.roundCounter++;
       this.specialAttackResetRound = this.roundCounter + 3;
       const attackDamage = _getRandomValue(10, 25);
       this.monsterHealth -= attackDamage;
+      this.battleLog.push(
+        "You special attack the monster, dealing " + attackDamage + " damage!"
+      );
       this.attackPlayer();
     },
     healPlayer() {
@@ -75,8 +99,10 @@ Vue.createApp({
       const healAmount = _getRandomValue(8, 15);
       if (this.playerHealth + healAmount > 100) {
         this.playerHealth = 100;
+        this.battleLog.push("You heal up, restoring to full health!");
       } else {
         this.playerHealth += healAmount;
+        this.battleLog.push("You heal up, restoring " + healAmount + " HP!");
       }
       this.attackPlayer();
     },
